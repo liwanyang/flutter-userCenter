6aa46853581f5ba94f1ab607a9e52c3104fd509f
yangwanli <newsuper6892@sina.com>
2021-08-27 17:30:47 +0800

    project reload

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# flutter_application_1
+# fultter_project
 
 A new Flutter project.
 
--- android/app/src/main/kotlin/com/example/fultter_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/fultter_project/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_application_1
+package com.example.fultter_project
 
 import io.flutter.embedding.android.FlutterActivity
 
