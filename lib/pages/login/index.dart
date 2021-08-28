024f08aad6a0d204f64971d3992965586bd2e878
yangwanli <ywl1049728030@163.com>
2021-08-28 20:31:04 +0800

    登陆页面开发

--- lib/pages/login/index.dart
+++ lib/pages/login/index.dart
@@ -8,7 +8,22 @@ class LoginView extends GetView<LoginController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('登录')),
-      body: Center(child: Text('登录页面')),
+      body: Container(padding: EdgeInsets.all(20),child: Column(children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(Icons.close),
+            TextButton(onPressed: () {
+              print(123);
+            }, child: Text('切换语言', style: TextStyle(color: Colors.grey[500]),))
+        ]),
+        Container(
+          child: Column(
+          children: [
+            Text('你好', style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16)),
+            Text('欢迎登陆UCore', style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16))
+        ])),
+      ]))
     );
   }
 }
